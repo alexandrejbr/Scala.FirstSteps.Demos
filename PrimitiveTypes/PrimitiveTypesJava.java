a0251bb731d892c36609211bbc20111ca8fadd1f
Alexandre Rodrigues <alexandrejbr@live.com>
2014-01-19 18:08:29 +0000

    Samples, primitive types

---
+++ PrimitiveTypes/PrimitiveTypesJava.java
@@ -0,0 +1,7 @@
+public class PrimitiveTypesJava {
+	public static void main(String[] args) {
+		int i = 1;
+		int i2 = i + 1;
+		System.out.println(i2);
+	}
+}
---
+++ PrimitiveTypes/primitivetypes.scala
@@ -0,0 +1,7 @@
+object PrimitiveTypesTests {
+	def main(args: Array[String]) = {
+		val i = 1
+		val i2 = i + 1
+		println(i2)
+	}
+}
